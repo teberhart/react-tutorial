6f0fbf46d243f3054ba7842525cda00dc450fef9
teberhart <thibaut.eberhart@gmail.com>
2024-02-05 10:09:29 +0100

    tutorial 43, didn't do 42 because I added status at the beginning

--- src/components/todo/Item.jsx
+++ src/components/todo/Item.jsx
@@ -3,18 +3,30 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faTrash} from "@fortawesome/free-solid-svg-icons";
 
 export default function Item({item, list, setList}) {
-    function handleDelete(text) {
-        setList(list.filter((todo)=> todo.text !== text));
+    function handleDelete(item) {
+        setList(list.filter((todo)=> todo !== item));
+    }
+
+    function handleToggle(item) {
+        //console.log("Status toggle for item ", item.text)
+        const newList = list.map(
+            (todo) => todo === item ?
+                {...todo, status: !(todo.status)} :
+                todo
+        );
+        setList(newList);
     }
 
     return (
         <li className={styles.itemContainer}>
             <div className={styles.item}>
-                {item.text}
+                <span onClick={() => handleToggle(item)} className={item.status ? styles.itemDone : ""}>
+                    {item.text}
+                </span>
                 <span>
                     <button
                         className={styles.deleteButton}
-                        onClick={() => handleDelete(item.text)}
+                        onClick={() => handleDelete(item)}
                     ><FontAwesomeIcon icon={faTrash}/></button>
                 </span>
             </div>
