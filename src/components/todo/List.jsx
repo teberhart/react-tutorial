0e1c9d840f921805185f3eb167e89ce8224920f9
teberhart <thibaut.eberhart@gmail.com>
2024-02-05 10:50:57 +0100

    tutorial 45

--- src/components/todo/List.jsx
+++ src/components/todo/List.jsx
@@ -2,10 +2,14 @@ import Item from "./Item.jsx";
 import styles from './List.module.css';
 
 export default function List({list, setList}) {
+    const sortedList = list.slice().sort(
+        (a, b) =>
+            Number(a.status) - Number(b.status)
+    );
     return (
         (list.length > 0) ?
             <ul className={styles.list}>
-                {list.map((item) => <Item key={item.text} item={item} list={list} setList={setList}/>)}
+                {sortedList.map((item) => <Item key={item.text} item={item} list={list} setList={setList}/>)}
             </ul>
         :
             <></>
